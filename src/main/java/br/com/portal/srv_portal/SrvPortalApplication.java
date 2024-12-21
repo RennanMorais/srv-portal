ca2ed2f6e251b67fe19bd568d705799e298db35a
Rennan Morais <rennan.morais@outloo.com>
2024-12-21 17:20:31 -0300

    base do projeto

---
+++ src/main/java/br/com/portal/srv_portal/SrvPortalApplication.java
@@ -0,0 +1,13 @@
+package br.com.portal.srv_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/portal/srv_portal/SrvPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portal.srv_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
